b18be7047db6781651362bfb1178715c46635149
Kevin Lutz <kevinlutz429@gmail.com>
2020-12-05 04:39:38 -0500

    updated password generator

--- projects/password-generator/password-generator.py
+++ projects/password-generator/password-generator.py
@@ -1,15 +1,18 @@
 import random
 
-lower = "abcdefghijklmnop"
-upper = "ABCDEFGHIJKLMNOP"
-symbols = "{}[]!?$(),."
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbols = "{}[]!?$(),.<>"
+numbers = "0123456789"
 
-all = lower + upper + symbols
-
+all = lower + upper + symbols + numbers
+print(all)
 print('-- Password Generator by Kevin Lutz --')
 while True:
+    password = ''
     length = input('\nEnter a length: ')
 
-    password = "".join(random.sample(all, int(length)))
+    for i in range(int(length)):
+        password += str(''.join(random.sample(all, 1)))
 
     print('Password generated:', password)
